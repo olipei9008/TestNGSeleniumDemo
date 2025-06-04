8578728149b881b5dfd50e475c3733c8df924c99
Oliver Pei <vespestas@gmail.com>
2025-06-04 01:47:36 -0700

    Added relevant Selenium code to check the stock symbols, and check if they match with an expected set of stock symbols.

--- src/main/java/GoogleFinance.java
+++ src/main/java/GoogleFinance.java
@@ -1,14 +1,81 @@
+import java.util.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class GoogleFinance {
     private WebDriver driver;
+    private List<String> symsList;
 
     public GoogleFinance(WebDriver driver) {
         this.driver = driver;
+        this.symsList = new ArrayList<>();
+        // Get the current "You may be interested in" stock symbols
+        String sectionClass = "sbnBtf";
+        String symbolClass = "COaKTb";
+        WebElement section = driver.findElement(By.className(sectionClass));
+        List<WebElement> elements = section.findElements(By.className(symbolClass));
+        for (int i = 0; i < elements.size(); i++) {
+            this.symsList.add(elements.get(i).getText());
+        }
     }
 
     public String getTitle() {
         return driver.getTitle();
+    }
+
+    public List<String> getInterestedSymbols() {
+        return symsList;
+    }
+
+    /*
+    This function does 3 things:
+    - Returns true if all the expected symbols are on the current page, false if not
+    - Prints the expected symbols NOT on the page
+    - Prints the page symbols NOT in the expected list
+     */
+    public boolean areExpectedSymbolsPresent(List<String> expectedSyms) {
+
+        int count = 0;
+
+        // Keep track of what to print
+        boolean[] printActual = new boolean[symsList.size()];
+        Arrays.fill(printActual, true);
+        boolean[] printExpected = new boolean[expectedSyms.size()];
+        Arrays.fill(printExpected, true);
+
+        //
+        for (int i = 0; i < expectedSyms.size(); i++) {
+            for (int j = 0; j < symsList.size(); j++) {
+                if (expectedSyms.get(i).equals(symsList.get(j))) {
+                   printActual[j] = false;
+                   printExpected[i] = false;
+                }
+            }
+        }
+
+        // Print symbols that weren't expected
+        System.out.println("List of symbols that weren't expected: ");
+        for (int i = 0; i < symsList.size(); i++) {
+            if (printActual[i]) {
+                 System.out.print(symsList.get(i) + " ");
+            }
+        }
+        System.out.println();
+
+        // Print expected symbols that weren't found
+        System.out.println("List of expected symbols that weren't found: ");
+        for (int i = 0; i < expectedSyms.size(); i++) {
+            if (printExpected[i]) {
+                System.out.print(expectedSyms.get(i) + " ");
+            }
+            else {
+                count++;
+            }
+        }
+        System.out.println();
+
+        System.out.println(count);
+        return count == expectedSyms.size();
     }
 }
--- src/test/java/SeleniumTests.java
+++ src/test/java/SeleniumTests.java
@@ -1,27 +1,58 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import java.util.*;
 
 public class SeleniumTests {
     private WebDriver driver;
 
     @BeforeMethod
     void setup() {
+        // Chrome driver path, replace with path to YOUR chrome web driver .exe
         String chromedriver_path = "C:\\WebDrivers\\chromedriver-win64\\chromedriver.exe";
+
+        // Create new headless instance of chromedriver
         System.setProperty("webdriver.chrome.driver", chromedriver_path);
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+
+        // Remove to see the browser
+        options.addArguments("--headless");
+
+        // Open the Google Finance page
+        driver = new ChromeDriver(options);
         driver.get("https://www.google.com/finance");
     }
 
     @Test
     void googleFinanceTitle() {
+        // Assert that we're on the Google Finance page
         String expected_title = "Google Finance - Stock Market Prices, Real-time Quotes & Business News";
-        GoogleFinance financepage = new GoogleFinance(driver);
-        Assert.assertEquals(financepage.getTitle(), expected_title);
+        GoogleFinance financePage = new GoogleFinance(driver);
+        Assert.assertEquals(financePage.getTitle(), expected_title);
+    }
+
+    @Test
+    void googleFinanceSymbols() {
+        // Assert we got six stock symbols and no more
+        GoogleFinance financePage = new GoogleFinance(driver);
+        List<String> stockSymbols = financePage.getInterestedSymbols();
+        System.out.println("List of Stock Symbols on Google Finance \"You may be interested in\":");
+        for (int i = 0; i < stockSymbols.size(); i++) {
+            System.out.print(stockSymbols.get(i) + " ");
+        }
+        System.out.println();
+        Assert.assertEquals(stockSymbols.size(), 6);
+    }
+
+    @Test
+    void unexpectedGoogleFinanceSymbols() {
+        GoogleFinance financePage = new GoogleFinance(driver);
+        List<String> mockExpectedSymbols = Arrays.asList("AAPL","INDEX");
+        Assert.assertTrue(financePage.areExpectedSymbolsPresent(mockExpectedSymbols));
     }
 
     @AfterMethod
